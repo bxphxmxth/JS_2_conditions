0c11d18c04c2b5747aad0001baef12494730e00a
Nasila <fushs@hotmail.fr>
2021-05-05 16:44:49 +0200

    js_2_conditions

---
+++ src/js/main.js
@@ -0,0 +1,85 @@
+
+
+// 1. OK
+
+// let rep1 = prompt("entre un nombre");
+// let rep2 = prompt("entre un autre");
+
+
+// if(rep1 == rep2){
+//     alert(`le nombre ${rep1} et ${rep2} sont-ils egaux? ${rep1 == rep2}`);
+// }else{
+//     alert(`le nombre ${rep1} et ${rep2} sont-ils egaux? ${rep1 == rep2}`);
+// }
+
+// 2. oki
+
+// let rep1 = prompt("ajoute un premier nombre");
+// let rep2 = prompt("ajoute un deuxieme nombre");
+
+// if(rep1 < rep2){
+//     alert(rep1 < rep2);
+// }else{
+//     alert(rep1 < rep2);
+// }
+
+
+// 3. 
+
+// let rep1 = prompt("ajoute un premier nombre");
+// let rep2 = prompt("ajoute un deuxieme nombre");
+// let rep3 = prompt("ajoute un troisième nombre");
+
+// if(rep1 + rep2 < rep3){
+//     alert(rep1 + rep2 < rep3)
+// }
+
+                    // alert("la somme du nombre " + rep1 + rep2 < rep3)
+
+// 4. OOK
+
+// let rep = prompt("enter ur sentence bro");
+// let estim = prompt("Estime le nombre de caractère de ta phrase");
+
+// let unites = Math.abs(estim - rep.length); 
+
+// if(rep.length == estim){
+//     alert("ui bravo");
+// }else{
+//     alert(`Faux, tu étais à ${unites} unités de la bonne réponse`);
+// }
+
+
+
+
+// 5. Okii
+
+// let prenom = prompt("ton préénoom bgg");
+
+// if(prenom){
+//     alert("merci bg");
+// }else{
+//     alert("entre ton prénom wesh ");
+// }
+
+// 6. WTF HELP
+
+
+// let luxe = "luxe";
+// console.log(luxe);
+// // let luxe = alert("Felicitation pour avoir choisis Luxe");
+// // let normal = alert("merci pour votre abonnement");
+
+
+// let rep = prompt("Veux-tu t'abonner?");
+
+
+// if(rep){
+//     prompt("Tu veux quelle formule, frère? Luxe ou Normal? ");
+
+// }if else()
+
+// 7.
+
+let quest1 = prompt("Où se trouve la mer noire?");
+let ques2 = prompt("Vat's à Yeet?");
